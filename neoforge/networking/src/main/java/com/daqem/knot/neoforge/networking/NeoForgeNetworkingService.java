bb622c958233f31e3e953991dd27c19202b19973
DAQEM <66999025+DAQEM@users.noreply.github.com>
2026-03-05 22:34:34 +0100

    feat: down port to 1.21.8

--- neoforge/networking/src/main/java/com/daqem/knot/neoforge/networking/NeoForgeNetworkingService.java
+++ neoforge/networking/src/main/java/com/daqem/knot/neoforge/networking/NeoForgeNetworkingService.java
@@ -11,6 +11,7 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.network.ClientPacketDistributor;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import org.jetbrains.annotations.NotNull;
@@ -47,7 +48,7 @@ public class NeoForgeNetworkingService implements NetworkingService {
 
     @Override
     public void sendToServer(CustomPacketPayload payload) {
-        PacketDistributor.sendToServer(payload);
+        ClientPacketDistributor.sendToServer(payload);
     }
 
     @Override
