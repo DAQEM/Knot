03ec7a770569799b9a5f814862635398a1cdeaee
DAQEM <66999025+DAQEM@users.noreply.github.com>
2026-04-25 22:25:10 +0200

    fix: creeper blow up crash neoforge Fixes #8

--- testmod-common/src/main/java/com/daqem/knot/test/network/ClientboundPayloadHandler.java
+++ testmod-common/src/main/java/com/daqem/knot/test/network/ClientboundPayloadHandler.java
@@ -1,13 +1,10 @@
 package com.daqem.knot.test.network;
 
 import com.daqem.knot.networking.ClientboundContext;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.PauseScreen;
 import org.jetbrains.annotations.NotNull;
 
 public class ClientboundPayloadHandler {
 
     public static void handle(@NotNull ClientboundPayload clientboundPayload, ClientboundContext clientboundContext) {
-        Minecraft.getInstance().setScreen(new PauseScreen(true));
     }
 }
